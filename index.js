d16a5fad4c228c91c2acc35d551d6c444d125ba8
raehyung <raehyung@raehyungs-MacBook-Pro.local>
2026-07-03 16:30:15 -0700

    More numbers

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { add, multiply } = require("test-repo-2");
 const _ = require("lodash");
 
-const numbers = [1, 2, 3, 4];
+const numbers = [1, 2, 3, 4, 5, 6, 7];
 
 const total = _.reduce(numbers, (acc, n) => add(acc, n), 0);
 const product = _.reduce(numbers, (acc, n) => multiply(acc, n), 1);
