539d4d360a0e6d8237f6cc91dba7637712e2bcc4
Raehyung Lee <raehyung-intern@ahum.ai>
2026-07-02 17:52:04 -0700

    change numbers

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const { add, multiply } = require("test-repo-2");
 const _ = require("lodash");
 
-const numbers = [1, 2, 3, 4];
+const numbers = [1, 2, 3, 4, 5, 6];
 
 const total = _.reduce(numbers, (acc, n) => add(acc, n), 0);
 const product = _.reduce(numbers, (acc, n) => multiply(acc, n), 1);
 
 console.log("sum:", total);
 console.log("product:", product);
+
