1ee3618041d89f2f47d2bc2916b19dcffb02b9d7
Raehyung Lee <151331539+RaehyungLee@users.noreply.github.com>
2026-07-09 16:26:52 -0700

    Use native Array.reduce for index.js aggregation

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const { add, multiply } = require("test-repo-2");
-const _ = require("lodash");
 
 const numbers = [1, 2, 3, 4];
 
-const total = _.reduce(numbers, (acc, n) => add(acc, n), 0);
-const product = _.reduce(numbers, (acc, n) => multiply(acc, n), 1);
+const total = numbers.reduce((acc, n) => add(acc, n), 0);
+const product = numbers.reduce((acc, n) => multiply(acc, n), 1);
 
 console.log("sum:", total);
 console.log("product:", product);
