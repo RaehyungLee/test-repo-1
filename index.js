51579e1b3141e1e621ca6559a170abeaef91527a
Raehyung Lee <151331539+RaehyungLee@users.noreply.github.com>
2026-07-09 15:32:15 -0700

    Compute index.js aggregation with an imperative loop

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const { add, multiply } = require("test-repo-2");
-const _ = require("lodash");
 
 const numbers = [1, 2, 3, 4];
 
-const total = _.reduce(numbers, (acc, n) => add(acc, n), 0);
-const product = _.reduce(numbers, (acc, n) => multiply(acc, n), 1);
+let total = 0;
+let product = 1;
+for (const n of numbers) {
+  total = add(total, n);
+  product = multiply(product, n);
+}
 
 console.log("sum:", total);
 console.log("product:", product);
