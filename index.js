6bf8f71c540ae7c9b30ee3785454ad35f872def4
Raehyung Lee <151331539+RaehyungLee@users.noreply.github.com>
2026-07-09 16:25:40 -0700

    Drop lodash from index.js aggregation, use native Array.reduce

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { add, multiply } = require("test-repo-2");
-const _ = require("lodash");
 
 const numbers = [1, 2, 3, 4];
 
-const total = _.reduce(numbers, (acc, n) => add(acc, n), 0);
-const product = _.reduce(numbers, (acc, n) => multiply(acc, n), 1);
+// Aggregate with the native Array.prototype.reduce instead of lodash.
+const total = numbers.reduce((acc, n) => add(acc, n), 0);
+const product = numbers.reduce((acc, n) => multiply(acc, n), 1);
 
 console.log("sum:", total);
 console.log("product:", product);
