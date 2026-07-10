eeb7f048ac1ff94f86be8587de0078d38cefbce8
raehyung0827 <arnold0827@icloud.com>
2026-07-10 11:30:05 -0700

    more numbers

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def multiply(a, b):
     return a * b
 
 
-numbers = [1, 2, 3, 4, 5]
+numbers = [1, 2, 3, 4, 5, 7, 8, 9, 11, 12, 13, 14]
 
 total = reduce(add, numbers, 0)
 product = reduce(multiply, numbers, 1)
